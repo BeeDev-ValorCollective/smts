41f676c36f243d5559088145c96af1544bcd8e8b
cgbridgewater <cgbridgewater@outlook.com>
2024-12-30 18:15:22 -0800

    build out the app jsx page with react router routes, hooks for getting/setting meta data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,120 @@
+// IMPORT REACT MAGIC
+import React, { Suspense } from 'react';
+import { BrowserRouter, Routes, Route, useLocation, Navigate } from 'react-router-dom';
 
+// IMPORT STYLES
 import './App.css'
 
-function App() {
+// IMPORT VIEWS (Lazy load for speed)
+const Index = React.lazy(() => import('./views/Index'));
+const Services = React.lazy(() => import('./views/Services'));
+const Contact = React.lazy(() => import('./views/Contact'));
+const About = React.lazy(() => import('./views/About'));
+const BadLink = React.lazy(() => import('./views/BadLink'));
 
+// IMPORT COMPONENTS
+import NavBar from './components/NavBar/Navbar';
+import Footer from './components/Footer/Footer';
+
+// Import custom hooks
+import useLoadMetaData from './customHooks/useLoadMetaData';
+import useUpdateMetaData from './customHooks/useUpdateMetaData';
+
+// WRAPPER TO UTILIZE THE LAZY LOADING
+function App() {
   return (
-    <>
-     
-    </>
-  )
+    <BrowserRouter>
+      <Suspense 
+        // this can be replaced with a spinner or something more exciting at some point
+        fallback={<div>Loading...</div>}
+      >
+        <AppContent />
+      </Suspense>
+    </BrowserRouter>
+  );
 }
 
-export default App
+// 
+function AppContent() {
+
+    // Fetch metadata from custom hook
+    const metaData = useLoadMetaData();
+
+    // Update document title and meta tags using custom hook
+    useUpdateMetaData(metaData);
+  
+
+  // USE LOCATION GETS URL NAME
+  const location = useLocation();
+
+  return (
+    <div className="app_wrapper">
+
+      {/* NAVBAR COMPONENT */}
+      <NavBar 
+        currentPath={location.pathname}
+      />
+
+      {/* ROUTES */}
+      <Routes>
+
+        {/* HOME PAGE */}
+        <Route 
+          path='/' 
+          element={
+            <Navigate to="/home" replace />
+          }
+        />
+
+        {/* LANDING PAGE */}
+        <Route 
+          path='/home'
+          element={
+            <Index />
+          }
+        />
+
+        {/* SERVICES PAGE */}
+        <Route 
+          path='/services'
+          element={
+            <Services />
+          }
+        />
+
+        {/* CONTACT PAGE */}
+        <Route 
+          path='/contact'
+          element={
+            <Contact />
+          }
+        />
+
+        {/* ABOUT */}
+        <Route 
+          path='/aboutus'
+          element={
+            <About />
+          }
+        />
+
+        {/* CATCH ALL FOR BAD LINKS */}
+        <Route 
+          path='*'
+          element={
+            <BadLink />
+          }
+        />
+
+      </Routes>
+
+      {/* FOOTER COMPONENT */}
+      <Footer 
+        currentPath={location.pathname}
+      />
+
+    </div>
+  );
+};
+
+export default App;
