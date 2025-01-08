100a724e6efa246f59eb1a165155e0322f053053
cgbridgewater <cgbridgewater@outlook.com>
2025-01-08 12:16:40 -0800

    add email, and update to use .env for phone and email on contact page

--- src/components/ContactComponents/ContactHeader/ContactHeader.jsx
+++ src/components/ContactComponents/ContactHeader/ContactHeader.jsx
@@ -3,9 +3,16 @@ import './contactHeader.css'
 
 // IMPORT IMAGES
 import Phone_Icon from '../../../assets/images/phone_icon.png'
+import Email_Icon from '../../../assets/images/email_icon.png'
 
 export default function ContactHeader() {
     
+    const PhoneLink = import.meta.env.VITE_LINK_PHONE;
+    const PhoneDisplay = import.meta.env.VITE_DISPLAY_PHONE;
+    const EmailLink = import.meta.env.VITE_INFO_EMAIL;
+
+
+
     return(
         <>
             <div className='contact_header'>
@@ -18,9 +25,23 @@ export default function ContactHeader() {
                 <p>
                     Call today to schedule your appointment or ask any questions you may have — Thad is here to help!
                 </p>
-                <img src={ Phone_Icon } alt="Call Us" />
-                <h3>PHONE</h3>
-                <h4>(804) 986-2425</h4>
+                <div className='contact_methods'>
+                    <div className='contact_type'>
+                        <img src={ Phone_Icon } alt="Call Us" />
+                        <h4>Call Us</h4>
+                        <a href={ PhoneLink }>
+                            <h5>{ PhoneDisplay }</h5>
+                        </a>
+                    </div>
+                    <div className='contact_type'>
+                        <img src={ Email_Icon } alt="Email Us" />
+                        <h4>Email Us</h4>
+                        <a href={`mailto:${ EmailLink }?subject=Request%20For%20Proposal&body=Hello,%0A%0AI%20would%20like%20to%20inquire%20about...`}>
+                            <h5>{ EmailLink }</h5>
+                        </a>
+                    </div>
+
+                </div>
             </div>
             <div className="message_hr">
                 <hr />
