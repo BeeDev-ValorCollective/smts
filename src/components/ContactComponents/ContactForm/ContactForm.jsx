f0a6e1dbbd01c158a5ad664a6a952e1270863daf
cgbridgewater <cgbridgewater@outlook.com>
2025-01-07 17:51:38 -0800

    contact form adjustments

--- src/components/ContactComponents/ContactForm/ContactForm.jsx
+++ src/components/ContactComponents/ContactForm/ContactForm.jsx
@@ -46,131 +46,129 @@ export default function ContactUs() {
     });
 
     return (
-        <div className="contact_wrapper">
-            <div className="contact_container">
-                {/* FORM SECTION */}
-                <form onSubmit={sendMail}>
-                    {/* TITLE SECTION */}
-                    <h2>Send us a Message</h2>
-                    {/* CLIENT NAME */}
-                    <div className="entry_area">
-                        <input
-                            name="userName"
-                            type="text"
-                            id="userName"
-                            required
-                            minLength={3}
-                            value={userName}
-                            onChange={(e) => setUserName(e.target.value)}
-                            placeholder="" // Leave blank!!
-                        />
-                        <label htmlFor="userName" className="label_line">
-                            Name:
-                        </label>
+        <div className="contact_container">
+            {/* FORM SECTION */}
+            <form onSubmit={sendMail}>
+                {/* TITLE SECTION */}
+                <h2>Send us a Message</h2>
+                {/* CLIENT NAME */}
+                <div className="entry_area">
+                    <input
+                        name="userName"
+                        type="text"
+                        id="userName"
+                        required
+                        minLength={3}
+                        value={userName}
+                        onChange={(e) => setUserName(e.target.value)}
+                        placeholder="" // Leave blank!!
+                    />
+                    <label htmlFor="userName" className="label_line">
+                        Name:
+                    </label>
+                </div>
+                {/* END CLIENT NAME */}
+                {/* CLIENT CONTACT EMAIL */}
+                <div className="entry_area">
+                    <input
+                        name="contact"
+                        type="email"
+                        id="contact"
+                        required
+                        value={contact}
+                        onChange={(e) => setContact(e.target.value)}
+                        placeholder="" // Leave blank!!
+                    />
+                    <label htmlFor="contact" className="label_line">
+                        Email:
+                    </label>
+                </div>
+                {/* END CLIENT CONTACT EMAIL */}
+                {/* CLIENT SUBJECT */}
+                <div className="entry_area">
+                    <input
+                        name="title"
+                        type="text"
+                        id="title"
+                        required
+                        minLength={3}
+                        value={subject}
+                        onChange={(e) => setSubject(e.target.value)}
+                        placeholder="" // Leave blank!!
+                    />
+                    <label htmlFor="title" className="label_line">
+                        Subject:
+                    </label>
+                </div>
+                {/* END CLIENT SUBJECT */}
+                {/* CLIENT MESSAGE */}
+                <div className="entry_area">
+                    <textarea
+                        name="description"
+                        id="description"
+                        required
+                        minLength={5}
+                        cols={10}
+                        rows={5}
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        placeholder="" // Leave blank!!
+                    />
+                    <label htmlFor="description" className="label_line">
+                        Message:
+                    </label>
+                </div>
+                {/* END CLIENT MESSAGE */}
+                {/* SUBMIT BUTTON AND SPINNER */}
+                {isSubmitting ? (
+                    <div className="form_button_box">
+                        {!mailError && !mailFail && !success ? (
+                            <ImSpinner10 className="spinner" />
+                        ) : (
+                            <>&nbsp;</>
+                        )}
                     </div>
-                    {/* END CLIENT NAME */}
-                    {/* CLIENT CONTACT EMAIL */}
-                    <div className="entry_area">
-                        <input
-                            name="contact"
-                            type="email"
-                            id="contact"
-                            required
-                            value={contact}
-                            onChange={(e) => setContact(e.target.value)}
-                            placeholder="" // Leave blank!!
-                        />
-                        <label htmlFor="contact" className="label_line">
-                            Email:
-                        </label>
+                ) : null}
+                {!isSubmitting && (
+                    <div className="form_button_box">
+                        <button hidden={!isButtonVisible} type="submit" disabled={!isFormValid}>
+                            Submit
+                        </button>
                     </div>
-                    {/* END CLIENT CONTACT EMAIL */}
-                    {/* CLIENT SUBJECT */}
-                    <div className="entry_area">
-                        <input
-                            name="title"
-                            type="text"
-                            id="title"
-                            required
-                            minLength={3}
-                            value={subject}
-                            onChange={(e) => setSubject(e.target.value)}
-                            placeholder="" // Leave blank!!
-                        />
-                        <label htmlFor="title" className="label_line">
-                            Subject:
-                        </label>
-                    </div>
-                    {/* END CLIENT SUBJECT */}
-                    {/* CLIENT MESSAGE */}
-                    <div className="entry_area">
-                        <textarea
-                            name="description"
-                            id="description"
-                            required
-                            minLength={5}
-                            cols={10}
-                            rows={5}
-                            value={message}
-                            onChange={(e) => setMessage(e.target.value)}
-                            placeholder="" // Leave blank!!
-                        />
-                        <label htmlFor="description" className="label_line">
-                            Message:
-                        </label>
-                    </div>
-                    {/* END CLIENT MESSAGE */}
-                    {/* SUBMIT BUTTON AND SPINNER */}
-                    {isSubmitting ? (
-                        <div className="form_button_box">
-                            {!mailError && !mailFail && !success ? (
-                                <ImSpinner10 className="spinner" />
-                            ) : (
-                                <>&nbsp;</>
-                            )}
-                        </div>
-                    ) : null}
-                    {!isSubmitting && (
-                        <div className="form_button_box">
-                            <button hidden={!isButtonVisible} type="submit" disabled={!isFormValid}>
-                                Submit
-                            </button>
+                )}
+                {/* END SUBMIT BUTTON AND SPINNER */}
+                {/* ERROR / SUCCESS MESSAGES */}
+                <div>
+                    {mailError && !mailFail && (
+                        <div className="mailer_messages">
+                            <h3>{mailError}</h3>
+                            <p>Please try again</p>
                         </div>
                     )}
-                    {/* END SUBMIT BUTTON AND SPINNER */}
-                    {/* ERROR / SUCCESS MESSAGES */}
-                    <div>
-                        {mailError && !mailFail && (
-                            <div className="mailer_messages">
-                                <h3>{mailError}</h3>
-                                <p>Please try again</p>
-                            </div>
-                        )}
-                        {mailFail && (
-                            <div className="mailer_messages failure">
-                                <h3>We are experiencing technical problems</h3>
-                                <p>Please contact us directly at -</p>
-                                <a
-                                    href={`mailto:${SupportEmail}?subject=Customer%20Contact%20Support&body=Hello,%0A%0AI%20would%20like%20to%20inquire%20about`}
-                                >
-                                    <b>{SupportEmail}</b>
-                                </a>
-                            </div>
-                        )}
-                        {success && (
-                            <div className="mailer_messages success">
-                                <h2>{success}</h2>
-                                <p>Redirecting you home...</p>
-                                <p>
-                                    Click <Link to="/">HERE</Link> if you are not redirected
-                                </p>
-                            </div>
-                        )}
-                    </div>
-                    {/* END ERROR / SUCCESS MESSAGES */}
-                </form>
-                {/* FORM SECTION */}
-            </div>
+                    {mailFail && (
+                        <div className="mailer_messages failure">
+                            <h3>We are experiencing technical problems</h3>
+                            <p>Please contact us directly at -</p>
+                            <a
+                                href={`mailto:${SupportEmail}?subject=Customer%20Contact%20Support&body=Hello,%0A%0AI%20would%20like%20to%20inquire%20about`}
+                            >
+                                <b>{SupportEmail}</b>
+                            </a>
+                        </div>
+                    )}
+                    {success && (
+                        <div className="mailer_messages success">
+                            <h2>{success}</h2>
+                            <p>Redirecting you home...</p>
+                            <p>
+                                Click <Link to="/">HERE</Link> if you are not redirected
+                            </p>
+                        </div>
+                    )}
+                </div>
+                {/* END ERROR / SUCCESS MESSAGES */}
+            </form>
+            {/* FORM SECTION */}
         </div>
     );
 }
